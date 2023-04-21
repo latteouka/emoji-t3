ba7a8da63974356be8216cb97e81e2889077aa0b
oukalatte <oukalatte@gmail.com>
2023-04-21 20:28:26 +0800

    add post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
-  const user = useUser();
+  const { user } = useUser();
+
+  const { data } = api.posts.getAll.useQuery();
+  console.log(data);
+
   return (
     <>
       <Head>
@@ -14,6 +19,11 @@ const Home: NextPage = () => {
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>{!user && <SignInButton />}</div>
         <div>{!!user && <SignOutButton />}</div>
+        <div>
+          {data?.map((post) => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
